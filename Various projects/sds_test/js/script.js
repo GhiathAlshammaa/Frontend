a939195c827ea46c2d9191398d55cd70a5c874c4
Ghiath2017 <g.alshamaa@hotmail.com>
2020-01-21 19:56:48 +0100

    Js done

--- Various projects/sds_test/js/script.js
+++ Various projects/sds_test/js/script.js
@@ -20,8 +20,19 @@ function ready() {
     $('.data-wrapper__loading').remove();
 
     // TODO We only need the vendors from globalVendorIds here.
+    /* The explaining : I've used a filter Method and instead the condition, 
+       which should normally exists with filter, I've puted a find Method to
+       returning only the items, which has same id in globalVendorIds Array */
+    const newVendorList = globalVendorList.filter((vendor) => {
+        return globalVendorIds.find((vendorId) => vendorId === vendor.id );
+    }); 
+
     // TODO Bonus: Vendors should be sorted alphabetically
-    let filteredGlobalVendorList = globalVendorList;
+    /* The localeCompare() method returns a number indicating whether a reference string 
+       comes before or after or is the same as the given string in sort order. */
+    newVendorList.sort((a,b) => a.name.localeCompare(b.name))
+
+    const filteredGlobalVendorList = newVendorList;
 
     addVendors(filteredGlobalVendorList);
 }
@@ -30,12 +41,24 @@ function addVendors(data) {
     for (let i = 0; i < data.length; i++) {
         const vendor = data[i];
 
-        // TODO HTML-Comment should be replaced with the names of all purposes for a vendor
+        // List contains the related purpose Ids
+        const purposeIdsList = data[i].purposeIds;
+        
+        // List contains the related purposes
+        let vendorPurposes = [];
 
+        // An operation to filtering the purposes List
+        purposes.filter((purpose) => {
+            // Filling the new List in purpose's name
+            purposeIdsList.forEach(element => { purpose.id === element ? vendorPurposes.push(purpose.name) : null });
+        });
+
+        // TODO HTML-Comment should be replaced with the names of all purposes for a vendor
         $(".data-wrapper").append(
             `<li class="vendors vendor-${vendor.id}">${vendor.name} (<a href="${vendor.policyUrl}" target="_blank">${vendor.policyUrl}</a>)
             <ul class="purposes">
-            <!-- TODO Render names of purposes here-->
+            <!-- TODO Render names of purposes here -->
+                ${ vendorPurposes.map((item) => `<li class='p-1'>${item}</li>`).join(' ') }
             </ul>
             </li>`
         );
