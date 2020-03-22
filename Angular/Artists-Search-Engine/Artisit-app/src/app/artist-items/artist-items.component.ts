a2bc3406be26868e0e96d98cda41ba8a705e0042
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-22 22:42:32 +0100

    added artist-item component

--- Angular/Artists-Search-Engine/Artisit-app/src/app/artist-items/artist-items.component.ts
+++ Angular/Artists-Search-Engine/Artisit-app/src/app/artist-items/artist-items.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-artist-items',
-  templateUrl: './artist-items.component.html',
-  styleUrls: ['./artist-items.component.css']
+  selector: "app-artist-items",
+  templateUrl: "./artist-items.component.html"
 })
 export class ArtistItemsComponent implements OnInit {
+  @Input() artist: Artist;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+}
 
-  ngOnInit(): void {
-  }
-
+interface Artist {
+  name: string;
+  shortname: string;
+  reknown: string;
+  bio: string;
 }
