dd39bcb8ef20e4d62742b338d36f24c1ae3a9ab5
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-30 12:14:16 +0200

    Product section has done

--- Angular/products-manager/src/app/app.module.ts
+++ Angular/products-manager/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from "./app-routing.module";
 import { RouterModule } from "@angular/router";
@@ -19,6 +22,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     RouterModule
