65ea82b58edf17c80018cd81072d5205d07604ad
Ghiath2017 <g.alshamaa@hotmail.com>
2020-03-30 10:38:48 +0200

    in app.module import BrowserAnimationsModule

--- Angular/products-manager/src/app/app.module.ts
+++ Angular/products-manager/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductsComponent } from './products/products.component';
-import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { ProductsComponent } from "./products/products.component";
+import { AboutComponent } from "./about/about.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -14,11 +15,8 @@ import { AboutComponent } from './about/about.component';
     ProductsComponent,
     AboutComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
