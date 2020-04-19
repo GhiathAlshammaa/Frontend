489fbbb4bd6c76b9eb31c1245a9079f924e09215
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-19 13:51:28 +0200

    last commint

--- Angular/products-manager/src/app/app.module.ts
+++ Angular/products-manager/src/app/app.module.ts
@@ -13,6 +13,12 @@ import { ProductsComponent } from "./products/products.component";
 import { AboutComponent } from "./about/about.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+/* Material */
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,7 +32,13 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule
+    RouterModule,
+    /* Material */
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
