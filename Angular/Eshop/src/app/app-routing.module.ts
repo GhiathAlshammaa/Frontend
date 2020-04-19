e225a725b08ca6cc96d326c39482f1f4246cee0b
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-19 16:47:28 +0200

    creat routing for productDetail

--- Angular/Eshop/src/app/app-routing.module.ts
+++ Angular/Eshop/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: 'products/:id', component: ProductDetailComponent},
+  {path: 'products', component: ProductListComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent},
-  {path: 'products', component: ProductListComponent},
 ];
 
 @NgModule({
