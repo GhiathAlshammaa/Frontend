63ac2aa597c6399f601542790b96e5d6690afc1a
Ghiath2017 <g.alshamaa@hotmail.com>
2020-07-02 20:40:11 +0200

    loading events

--- React/react-redux-ts/src/redux/user-events.ts
+++ React/react-redux-ts/src/redux/user-events.ts
@@ -1,23 +1,26 @@
 import { AnyAction, Action } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { RootState } from './store';
+import { selectDateStart } from './recorder';
 
-interface UserEvent {
+export interface UserEvent {
   id: number;
   title: string;
   dateStart: string;
   dateEnd: string;
 }
 
-interface userEventsState {
+interface UserEventsState {
   byIds: Record<UserEvent['id'], UserEvent>;
   allIds: UserEvent['id'][];
 }
 
-const LOAD_REQUEST = 'userEvents/load_success';
-interface LoadRequestAction extends Action<typeof LOAD_REQUEST> {}
+const LOAD_REQUEST = 'userEvents/load_request';
+
+interface LoadReaquestAction extends Action<typeof LOAD_REQUEST> {}
 
 const LOAD_SUCCESS = 'userEvents/load_success';
+
 interface LoadSuccessAction extends Action<typeof LOAD_SUCCESS> {
   payload: {
     events: UserEvent[];
@@ -25,6 +28,7 @@ interface LoadSuccessAction extends Action<typeof LOAD_SUCCESS> {
 }
 
 const LOAD_FAILURE = 'userEvents/load_failure';
+
 interface LoadFailureAction extends Action<typeof LOAD_FAILURE> {
   error: string;
 }
@@ -33,7 +37,7 @@ export const loadUserEvents = (): ThunkAction<
   void,
   RootState,
   undefined,
-  LoadRequestAction | LoadSuccessAction | LoadFailureAction
+  LoadReaquestAction | LoadSuccessAction | LoadFailureAction
 > => async (dispatch, getState) => {
   dispatch({
     type: LOAD_REQUEST,
@@ -55,6 +59,61 @@ export const loadUserEvents = (): ThunkAction<
   }
 };
 
+const CREATE_REQUEST = 'userEvents/create_request';
+
+interface CreateRequestAction extends Action<typeof CREATE_REQUEST> {}
+
+const CREATE_SUCCESS = 'userEvents/create_success';
+
+interface CreateSuccessAction extends Action<typeof CREATE_SUCCESS> {
+  payload: {
+    event: UserEvent;
+  };
+}
+
+const CREATE_FAILURE = 'userEvents/create_failure';
+
+interface CreateFailureAction extends Action<typeof CREATE_FAILURE> {}
+
+export const createUserEvent = (): ThunkAction<
+  Promise<void>,
+  RootState,
+  undefined,
+  CreateRequestAction | CreateSuccessAction | CreateFailureAction
+> => async (dispatch, getState) => {
+  dispatch({
+    type: CREATE_REQUEST,
+  });
+
+  try {
+    const dateStart = selectDateStart(getState());
+    const event: Omit<UserEvent, 'id'> = {
+      title: 'No name',
+      dateStart,
+      dateEnd: new Date().toISOString(),
+    };
+
+    const response = await fetch(`http://localhost:3001/events`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(event),
+    });
+
+    const createdEvent: UserEvent = await response.json();
+
+    dispatch({
+      type: CREATE_SUCCESS,
+      payload: { event: createdEvent },
+    });
+  } catch (e) {
+    dispatch({
+      type: CREATE_FAILURE,
+    });
+  }
+};
+
 const selectUserEventsState = (rootState: RootState) => rootState.userEvents;
 
 export const selectUserEventsArray = (rootState: RootState) => {
@@ -62,13 +121,14 @@ export const selectUserEventsArray = (rootState: RootState) => {
   return state.allIds.map((id) => state.byIds[id]);
 };
 
-const initialState: userEventsState = {
+const initialState: UserEventsState = {
   byIds: {},
   allIds: [],
 };
+
 const userEventsReducer = (
-  state: userEventsState = initialState,
-  action: LoadSuccessAction
+  state: UserEventsState = initialState,
+  action: LoadSuccessAction | CreateSuccessAction
 ) => {
   switch (action.type) {
     case LOAD_SUCCESS:
@@ -76,11 +136,20 @@ const userEventsReducer = (
       return {
         ...state,
         allIds: events.map(({ id }) => id),
-        byIds: events.reduce<userEventsState['byIds']>((byIds, event) => {
+        byIds: events.reduce<UserEventsState['byIds']>((byIds, event) => {
           byIds[event.id] = event;
           return byIds;
         }, {}),
       };
+
+    case CREATE_SUCCESS:
+      const { event } = action.payload;
+      return {
+        ...state,
+        allIds: [...state.allIds, event.id],
+        byIds: { ...state.byIds, [event.id]: event },
+      };
+
     default:
       return state;
   }
