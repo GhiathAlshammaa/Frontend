c2f397f40e524f521d1dcbc94c85bc059210555c
Ghiath2017 <g.alshamaa@hotmail.com>
2020-11-15 17:11:15 +0100

    add a new component

--- Angular/CRUD/Employees-System-v1/src/app/app.module.ts
+++ Angular/CRUD/Employees-System-v1/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditEmployeeComponent } from './employees/edit-employee/edit-employee.component';
+import { EmployeeDetailsComponent } from './employees/employee-details/employee-details.component';
+import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeesListComponent
   ],
   imports: [
     BrowserModule,
