293aaf3d3dd01c3aed102ee398b2f05780f7e5eb
Ghiath2017 <g.alshamaa@hotmail.com>
2020-11-15 22:13:09 +0100

    fix the routing

--- Angular/CRUD/Employees-System-v1/src/app/app-routing.module.ts
+++ Angular/CRUD/Employees-System-v1/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmployeeDetailsGuard } from './employees/employee-details.guard';
 import { EmployeeDetailsComponent } from './employees/employee-details/employee-details.component';
 import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { WelcomePapeComponent } from './welcome-pape/welcome-pape.component';
 
 const routes: Routes = [
   { path: 'home', component: WelcomePapeComponent },
   { path: 'employees-list', component: EmployeesListComponent },
-  { path: 'employee-details/:id', canActivate: [EmployeeDetailsGuard], component: EmployeeDetailsComponent },
+  {
+    path: 'employee-details/:id',
+    canActivate: [EmployeeDetailsGuard],
+    component: EmployeeDetailsComponent,
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
