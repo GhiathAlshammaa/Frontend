1700f11db675b80db91b218bab505fed5f87f0c2
Ghiath2017 <g.alshamaa@hotmail.com>
2020-11-25 14:09:41 +0100

    update Angular Folder

--- Angular/vocabulary-bank/src/app/word/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './create-word/create-word.component';
-export * from './update-word/update-word.component';
-export * from './word-detail/word-detail.component';
-export * from './word-list/word-list.component';
-export * from './create-word/create-word.component';
--- Angular/vocabulary-bank/src/app/word/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  WordListComponent,
-  WordDetailComponent,
-  CreateWordComponent,
-  UpdateWordComponent
-} from './components/index';
-export * from './word-routing.module';
