6437ff9e71df27376a803222f50f488b96ec8555
Ghiath2017 <g.alshamaa@hotmail.com>
2020-11-25 17:01:37 +0100

    Add a handled event, that changes the style of the form after clicking

--- Angular/template-driven/src/app/app.component.ts
+++ Angular/template-driven/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'template-driven';
+  formMode = true;
+  signInForm = 'container';
+  signUpForm = 'container sign-up-mode';
+  ngOnInit(): void {}
+
+  onClickSingUp() {
+    this.formMode = !this.formMode;
+    console.log(this.formMode);
+  }
+
+  onClickSingIn() {
+    this.formMode = !this.formMode;
+    console.log(this.formMode);
+  }
 }
