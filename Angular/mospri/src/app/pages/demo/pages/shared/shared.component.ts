3dd01342f23e524620a94e0e126a68cf78792f89
Ghiath2017 <g.alshamaa@hotmail.com>
2021-02-24 16:45:06 +0100

    add password field and form field

--- Angular/mospri/src/app/pages/demo/pages/shared/shared.component.ts
+++ Angular/mospri/src/app/pages/demo/pages/shared/shared.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { regex, regexErrors } from '@app/shared/utils';
 
 @Component({
   selector: 'mospri-shared',
@@ -9,18 +10,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SharedComponent implements OnInit {
   form: FormGroup;
   isInline: boolean;
+  regexErrors = regexErrors;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.isInline = true;
+  }
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      input: [null, {
-        updateOn: 'blur',
-        validators: [
-          Validators.required,
-          Validators.minLength(3)
-        ]
-      }]
+      input: [
+        null,
+        {
+          updateOn: 'blur',
+          validators: [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.pattern(regex.numbers),
+          ],
+        },
+      ],
+      password: [
+        null,
+        {
+          updateOn: 'blur',
+          validators: [Validators.required],
+        },
+      ],
     });
   }
 
