43f73851771f01040a569b215184c4f918f2baf6
Ghiath2017 <g.alshamaa@hotmail.com>
2021-02-24 22:33:19 +0100

    add select and checkboxes elments

--- Angular/mospri/src/app/pages/demo/pages/shared/shared.component.ts
+++ Angular/mospri/src/app/pages/demo/pages/shared/shared.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { regex, regexErrors } from '@app/shared/utils';
 
+import { ControlItem } from '@app/models/frontend';
 @Component({
   selector: 'mospri-shared',
   templateUrl: './shared.component.html',
@@ -12,8 +13,18 @@ export class SharedComponent implements OnInit {
   isInline: boolean;
   regexErrors = regexErrors;
 
+  items: ControlItem[];
+
   constructor(private fb: FormBuilder) {
     this.isInline = true;
+
+    this.items = [
+      { label: 'First', value: 1 },
+      { label: 'Second', value: 2 },
+      { label: 'Third', value: 3 },
+      { label: 'Fourth', value: 4 },
+      { label: 'Fifth', value: 5 },
+    ];
   }
 
   ngOnInit(): void {
@@ -33,6 +44,20 @@ export class SharedComponent implements OnInit {
         null,
         {
           updateOn: 'blur',
+          validators: [Validators.required],
+        },
+      ],
+      select: [
+        null,
+        {
+          updateOn: 'change',
+          validators: [Validators.required],
+        },
+      ],
+      checkboxes: [
+        null,
+        {
+          updateOn: 'change',
           validators: [Validators.required],
         },
       ],
