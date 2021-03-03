df6ef8885a887b6a2ad1bf454eb0a22a9ee1d257
Ghiath2017 <g.alshamaa@hotmail.com>
2021-03-03 14:54:47 +0100

    new update

--- Typescript/training-project-1/build/server.js
+++ Typescript/training-project-1/build/server.js
@@ -6,3 +6,4 @@ var app = express();
 app.listen(port);
 app.use(express.static('public')); // to randering index file
 console.log("Listening on Port " + port);
+//# sourceMappingURL=server.js.map
--- Typescript/training-project-1/server/server.ts
+++ Typescript/training-project-1/server/server.ts
@@ -1,9 +1,27 @@
 import * as express from 'express';
+import { Question } from '../@types/Question';
+import * as path from 'path';
+
+const questions: Question[] = [
+  {
+    title: 'How to log in?',
+    content: 'How do I log in?',
+    answerCount: 2,
+  },
+];
 
 const port: string | number = process.env.port || 1122;
 const app = express();
 
 app.listen(port);
-app.use(express.static('public')); // to randering index file
+app.use(express.static('public')); // to rendering index file
 
-console.log(`Listening on Port ` + port);
+console.log(`Listening on ` + port);
+
+app.get('/questions', (_req, res) => {
+  res.json(questions);
+});
+
+app.get('/main.js', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'client.js'));
+});
