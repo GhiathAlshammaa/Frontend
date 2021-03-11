05259c155eaad08645e216dae24f940a445aa63f
Ghiath2017 <g.alshamaa@hotmail.com>
2021-03-11 16:09:10 +0100

    add email-confirm and store init for User

--- Angular/mospri/src/app/app.component.ts
+++ Angular/mospri/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 import { Store } from '@ngrx/store';
 import * as fromRoot from './store';
+import * as fromUser from './store/user';
 import * as fromDictionaries from './store/dictionaries';
 
 @Component({
@@ -16,6 +17,7 @@ export class AppComponent implements OnInit {
   constructor(private store: Store<fromRoot.State>) {}
 
   ngOnInit() {
+    this.store.dispatch(new fromUser.Init());
     this.store.dispatch(new fromDictionaries.Read());
   }
 
