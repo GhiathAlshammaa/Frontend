76a345f106a41ed639844546ed6ea44acbe2970b
Ghiath2017 <g.alshamaa@hotmail.com>
2021-03-24 18:59:42 +0100

    add packages and firebase config

--- Angular/workspace0/projects/todo/src/environments/environment.prod.ts
+++ Angular/workspace0/projects/todo/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const env = {
   production: true,
-  name: 'prod'
+  name: 'prod',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyB6JJKSeQakGo8sbVoFcYCqRCqBsDKQSGI',
+      authDomain: 'todo-65156.firebaseapp.com',
+      projectId: 'todo-65156',
+      storageBucket: 'todo-65156.appspot.com',
+      messagingSenderId: '921561045655',
+      appId: '1:921561045655:web:916aeefa47eef17d5b27d2',
+    },
+  },
 };
--- Angular/workspace0/projects/todo/src/environments/environment.ts
+++ Angular/workspace0/projects/todo/src/environments/environment.ts
@@ -4,7 +4,17 @@
 
 export const env = {
   production: false,
-  name: 'default'
+  name: 'default',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyB6JJKSeQakGo8sbVoFcYCqRCqBsDKQSGI',
+      authDomain: 'todo-65156.firebaseapp.com',
+      projectId: 'todo-65156',
+      storageBucket: 'todo-65156.appspot.com',
+      messagingSenderId: '921561045655',
+      appId: '1:921561045655:web:916aeefa47eef17d5b27d2',
+    },
+  },
 };
 
 /*
