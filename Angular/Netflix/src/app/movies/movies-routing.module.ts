81ba08c09320e0b411bb29b2a74163fc7344af6c
Ghiath2017 <g.alshamaa@hotmail.com>
2021-04-22 13:25:39 +0200

    add child route for movies pages

--- Angular/Netflix/src/app/movies/movies-routing.module.ts
+++ Angular/Netflix/src/app/movies/movies-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movie:id', component: MovieDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MoviesRoutingModule { }
+export class MoviesRoutingModule {}
