1644812f92de861831fe9373749336cc5c1945f0
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-04 13:08:00 +0200

    Organized the Movies service to be more clean and readable

--- Angular/Netflix/src/app/core/services/movies.service.ts
+++ Angular/Netflix/src/app/core/services/movies.service.ts
@@ -1,23 +1,30 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '@src/environments/environment';
-import { catchError, filter, map, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { Movie } from '../models/movie';
-import { ExtractData, HandleError, SubtractDates } from '../utils';
+import {
+  ExtractData,
+  HandleError,
+  SubtractDates,
+  UrlGenerator,
+} from '../utils';
 import * as moment from 'moment';
-import { combineLatest, forkJoin, Observable } from 'rxjs';
 import { Country } from '../models/country';
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService implements OnInit {
-  language = 'en-US';
-  today = moment.now();
-  urlUpcoming = `${environment.apiConfig.url}upcoming?api_key=${environment.apiConfig.apikey}&language=${this.language}&page=1`;
-  urlCountries = `${environment.apiConfig.urlConfig}countries?api_key=${environment.apiConfig.apikey}`;
+  // Url Properties for Url Generator
+  language = '';
+  pageNum;
+  restUrlValue = '';
+  today;
+  urlCountries = '';
+  urlUpComing = '';
   countries: Country[] = [];
 
-  movies$ = this.http.get<Movie[]>(this.urlUpcoming).pipe(
+  // Array contains all the Movies in "UpComing" Section
+  movies$ = this.http.get<Movie[]>(this.urlUpComing).pipe(
     map((data) => ExtractData(data)),
     map((movies) =>
       movies.map(
@@ -33,31 +40,28 @@ export class MoviesService implements OnInit {
     catchError(HandleError)
   );
 
-  getMovieById(movieId: number): Observable<Movie> {
-    const urlMovieById = `${environment.apiConfig.url}${movieId}?api_key=${environment.apiConfig.apikey}&language=${this.language}`;
-    return this.http.get<Movie>(urlMovieById).pipe(
-      // tap((movies) => console.log(movies)),
-      catchError(HandleError)
-    );
-  }
-
-  // getProductionCountriesByMovieId(movieId: number): void {
-  //   console.log(`movieId: ${movieId}`);
-  // return this.getMovieById(movieId).pipe(
-  //   map((movie) => {
-  //     this.countries = movie.production_countries;
-  //     // console.log(`movie: ${movie}`);
-  //     return movie.production_countries;
-  //   })
-  // );
-  // }
-
   countries$ = this.http.get<Country[]>(this.urlCountries).pipe(
     tap((country) => console.log(country)),
     catchError(HandleError)
   );
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // Url Params initialization
+    this.language = 'en-US';
+    this.pageNum = 1;
+    this.restUrlValue = `&language=${this.language}&page=${this.pageNum}`;
+
+    // Generating Url
+    this.urlCountries = UrlGenerator('config', 'countries');
+    this.urlUpComing = UrlGenerator('normal', 'upcoming', this.restUrlValue);
+
+    // Bringing a today date, in order to know since when the Movie released
+    this.today = moment.now();
+
+    // Test Section
+    // console.log(`urlUpComing: ${this.urlUpComing}`);
+    // console.log(`urlCountries: ${this.urlCountries}`);
+  }
 
   ngOnInit() {}
 }
