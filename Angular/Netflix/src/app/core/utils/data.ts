b52ed516417f7d75228f44225e020877fe4f6854
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-05 12:42:02 +0200

    create ExtractDataCredits to collect all staff in an Array

--- Angular/Netflix/src/app/core/utils/data.ts
+++ Angular/Netflix/src/app/core/utils/data.ts
@@ -1,9 +1,22 @@
+import { Staff } from '../models';
+
 export const ExtractData = (res: any) => {
   let body = res;
 
   // Check the Movies Array in the Console
   // console.log(body);
   return body.results || {};
+};
+export const ExtractDataCredits = (res: any): Staff[] => {
+  let body = res;
+  let staff: Staff[];
+
+  // Check the Movies Array in the Console
+  staff = body.cast;
+  staff.push(...staff, ...body.crew);
+  // console.log(`Staff: ${JSON.stringify(staff)}`);
+
+  return staff;
 };
 export const SubtractDates = (date): number => {
   const today = new Date();
