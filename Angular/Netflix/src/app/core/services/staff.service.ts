19cbe674628ec5a02a78886e19d0ff39f9189e89
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-05 12:41:02 +0200

    change the name of service to staff

---
+++ Angular/Netflix/src/app/core/services/staff.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Credit, Staff } from '../models';
+import { HandleError, UrlGenerator } from '../utils';
+import { ExtractDataCredits } from '../utils/data';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StaffService {
+  // Url Params initialization
+  language = 'en-US';
+  restUrlValue = `&language=${this.language}`;
+  urlCredits = '';
+
+  // Variable contains a required Credits
+  credits$ = (movieId: number): Observable<Staff[]> => {
+    this.urlCredits = UrlGenerator(
+      'normal',
+      'movie/' + movieId,
+      'credits',
+      this.restUrlValue
+    );
+    // console.log(`urlCredits: ${this.urlCredits}`);
+    
+    return this.http.get<Staff[]>(this.urlCredits).pipe(
+      map((credits) => ExtractDataCredits(credits)),
+      // tap((staff)=> console.log(staff)),
+      catchError(HandleError)
+    );
+  };
+
+  constructor(private http: HttpClient) {}
+}
