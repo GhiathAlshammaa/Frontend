bdc86e5e589fcf965ee64cec56b699d8341ea203
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-05 12:45:44 +0200

    add urlBase instead of url in env

--- Angular/Netflix/src/environments/environment.dev.ts
+++ Angular/Netflix/src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
   name: 'dev',
   apiConfig: {
     apikey: '6d63ff2905593d362cd65d9b4367d4d6',
-    url: 'https://api.themoviedb.org/3/movie/',
+    urlBase: 'https://api.themoviedb.org/3/',
     urlConfig: 'https://api.themoviedb.org/3/configuration/',
   },
 };
