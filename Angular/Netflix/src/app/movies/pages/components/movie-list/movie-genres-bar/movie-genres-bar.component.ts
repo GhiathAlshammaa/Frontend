ee4596d1c9d54c512988ba43d8bb95590cfea65e
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-10 19:28:34 +0200

    fix route in genres route & clean up the logic

--- Angular/Netflix/src/app/movies/pages/components/movie-list/movie-genres-bar/movie-genres-bar.component.ts
+++ Angular/Netflix/src/app/movies/pages/components/movie-list/movie-genres-bar/movie-genres-bar.component.ts
@@ -10,41 +10,21 @@ import { catchError } from 'rxjs/operators';
   styleUrls: ['movie-genres-bar.component.scss'],
 })
 export class MovieGenresBarComponent implements OnInit {
+  errorMsg = '';
+
   genres$ = this.moviesService.genres$.pipe(
     catchError((err) => {
       this.errorMsg = err;
       return EMPTY;
     })
   );
-  moviesGenreById$;
-  genreId = 99;
-  pageTitle = this.route.snapshot.data['pageTitle'] === 'g' ? 1 : 0;
 
-  errorMsg = '';
-  constructor(
-    private moviesService: MoviesService,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
+  constructor(private moviesService: MoviesService, private router: Router) {
     // this is for routerLink on same component when only queryParameter changes
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false;
     };
   }
 
-  ngOnInit(): void {
-    this.genreId = +this.route.snapshot.paramMap.get(':id');
-    console.log(`genreId: ${this.genreId}`);
-
-    // GenreId always appear 0 !!!
-
-    // this.moviesGenreById$ = this.moviesService
-    //   .moviesGenreById$(this.genreId)
-    //   .pipe(
-    //     catchError((err) => {
-    //       this.errorMsg = err;
-    //       return EMPTY;
-    //     })
-    //   );
-  }
+  ngOnInit(): void {}
 }
