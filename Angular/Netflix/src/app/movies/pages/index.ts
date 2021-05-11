60c7b1710e2daacea29188281e61f865c3476da1
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-11 15:45:17 +0200

    create actor route

--- Angular/Netflix/src/app/movies/pages/index.ts
+++ Angular/Netflix/src/app/movies/pages/index.ts
@@ -1,5 +1,7 @@
 export { MovieListComponent } from './movie-list/movie-list.component';
 export { MovieDetailComponent } from './movie-detail/movie-detail.component';
+export { ActorDetailComponent } from './actor-detail/actor-detail.component';
+
 export {
   MovieExternalInfoComponent,
   MovieInternalInfoComponent,
