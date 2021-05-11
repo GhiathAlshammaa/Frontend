3ce7aa9d6aa824ed5c703d60eb5bb553596fedda
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-11 16:10:51 +0200

    establish the binding between component and service in actor page

--- Angular/Netflix/src/app/core/services/staff.service.ts
+++ Angular/Netflix/src/app/core/services/staff.service.ts
@@ -1,19 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
-import { Staff } from '../models';
+import { Actor, Staff } from '../models';
 import { HandleError, UrlGenerator } from '../utils';
 import { ExtractDataCredits } from '../utils/data';
 
 @Injectable({
   providedIn: 'root',
 })
-export class StaffService {
+export class StaffService implements OnInit {
   // Url Params initialization
   language = 'en-US';
   restUrlValue = `&language=${this.language}`;
   urlStaff = '';
+  urlActor = '';
+
+  actorById$ = (actorId: number): Observable<Actor> => {
+    this.urlActor = UrlGenerator(
+      'normal',
+      'person',
+      actorId,
+      this.restUrlValue
+    );
+    // console.log(`urlActor: ${this.urlActor}`);
+    return this.http.get<Actor>(this.urlActor).pipe(
+      tap((actor) => console.log(actor)),
+      catchError(HandleError)
+    );
+  };
 
   // Variable contains a required Credits
   staff$ = (movieId: number): Observable<Staff[]> => {
@@ -32,4 +47,5 @@ export class StaffService {
   };
 
   constructor(private http: HttpClient) {}
+  ngOnInit(): void {}
 }
--- Angular/Netflix/src/app/movies/pages/actor-detail/actor-detail.component.ts
+++ Angular/Netflix/src/app/movies/pages/actor-detail/actor-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Actor } from '@app/core/models';
+import { StaffService } from '@app/core/services';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-actor-detail',
@@ -6,7 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['actor-detail.component.scss'],
 })
 export class ActorDetailComponent implements OnInit {
-  constructor() {}
+  actorById$;
+  actorId = 0;
+  errorMsg: any;
+  constructor(
+    private staffService: StaffService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.actorId = +this.route.snapshot.paramMap.get('id');
+    // console.log(`actorId: ${this.actorId}`);
+
+    this.actorById$ = this.staffService.actorById$(this.actorId).pipe(
+      catchError((err) => {
+        this.errorMsg = err;
+        return EMPTY;
+      })
+    );
+    this.actorById$.subscribe();
+  }
 }
