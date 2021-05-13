af154a41370c5b5175290905434cda4c187584b1
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-13 07:55:25 +0200

    extend movie-detail com to have streaming binding

--- Angular/Netflix/src/app/movies/pages/movie-detail/movie-detail.component.ts
+++ Angular/Netflix/src/app/movies/pages/movie-detail/movie-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from '@app/core/models/movie';
 import { MovieService, MoviesService } from '@app/core/services';
+import { StreamingService } from '@app/core/services/streaming.service';
 import { UrlGenerator } from '@app/core/utils';
 import { EMPTY, Observable } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
@@ -15,15 +16,17 @@ export class MovieDetailComponent implements OnInit {
   errorMessage = '';
   movie$: Observable<Movie>;
   movieSimilar$: Observable<Movie[]>;
+  streaming$;
   id = 0;
 
   constructor(
     private route: ActivatedRoute,
-    private movieService: MovieService
+    private movieService: MovieService,
+    private streamingService: StreamingService
   ) {
     this.id = +this.route.snapshot.paramMap.get('id');
   }
-  ngOnInit(): void {
+  async ngOnInit() {
     this.movie$ = this.movieService.movie$(this.id).pipe(
       // tap((data) => console.log(data)),
       catchError((err) => {
@@ -31,5 +34,17 @@ export class MovieDetailComponent implements OnInit {
         return EMPTY;
       })
     );
+
+    await this.streamingService.getUserCountry();
+
+    this.streaming$ = this.streamingService
+      .streamingByMovieId$(501929, this.streamingService.countryCode)
+      .pipe(
+        catchError((err) => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      );
+    this.streaming$.subscribe();
   }
 }
