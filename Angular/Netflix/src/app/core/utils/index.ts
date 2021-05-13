f1412fc4df12a23996a2fd42c930012150ea6c96
Ghiath2017 <g.alshamaa@hotmail.com>
2021-05-13 07:40:50 +0200

    extend barrel file in utils

--- Angular/Netflix/src/app/core/utils/index.ts
+++ Angular/Netflix/src/app/core/utils/index.ts
@@ -3,3 +3,4 @@ export { HandleError } from './error';
 export { ExtractData } from './data';
 export { UrlGenerator } from './url';
 export { SubtractDates, YearOfDate } from './date';
+export { CountryName } from './country';
